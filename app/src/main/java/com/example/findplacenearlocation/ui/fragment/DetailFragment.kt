7cba93c5e833bcf5afdc08dc0551c043d61fcf9d
gap <omidzakeri22@gmail,com>
2022-04-13 16:54:32 +0430

    commit by omid

--- app/src/main/java/com/example/findplacenearlocation/ui/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/findplacenearlocation/ui/fragment/DetailFragment.kt
@@ -1,6 +1,12 @@
 package com.example.findplacenearlocation.ui.fragment
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.findplacenearlocation.ui.viewmodel.VenueViewModel
 
 class DetailFragment : Fragment(){
+
+    lateinit var viewModel: VenueViewModel
+    val args : DetailFragmentArgs by navArgs()
+
 }
